642e40e1a6059536e4528698a91079a20428a521
deveb1479@gmail.com <deveb1479@gmail.com>
2022-09-14 16:02:53 +0900

    challege

---
+++ ScreenShot/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenShot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ((Bitmap)Clipboard.GetImage()).Save(Environment.SpecialFolder.DesktopDirectory + "\\ScreenShot.png");
+            this.Close();
+        }
+    }
+}
