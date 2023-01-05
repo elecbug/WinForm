c739014cd857454bbcf42e49a6b5112b0902985a
deveb1479@gmail.com <deveb1479@gmail.com>
2023-01-06 00:12:25 +0900

    I'm winner

--- GBTH/List/PartedIngredientList.cs
+++ GBTH/List/PartedIngredientList.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GBTH.List
 {
-    public partial class PartedIngredientList : ListView
+    public class PartedIngredientList : ListView
     {
         public class Row
         {
@@ -17,12 +17,51 @@ namespace GBTH.List
             public string? Unit { get; set; }
             public string? Assortment { get; set; }
         }
+        public class Report
+        {
+            public int Number { get; set; }
+            public string? Name { get; set; }
+            public string? Standard { get; set; }
+            public List<ReportRow> Rows { get; set; } = new List<ReportRow>();
+        }
+        public class ReportRow
+        {
+            public int Number { get; set; }
+            public string? Date { get; set; }
+            public string? Descryption { get; set; }
+            public int Insert { get; set; }
+            public int Use { get; set; }
+            public int Storege { get; set; }
+            public string? Manager { get; set; }
+            public string? Other { get; set; }
+
+            public ReportRow(int number, string? date, string? descryption, int insert,
+                int use, int storege, string? manager, string? other)
+            {
+                this.Number = number;
+                this.Date = date;
+                this.Descryption = descryption;
+                this.Insert = insert;
+                this.Use = use;
+                this.Storege = storege;
+                this.Manager = manager;
+                this.Other = other;
+            }
+        }
 
         public List<Row> Rows { get; private set; }
+        public List<Report> Reports { get; private set; }
 
-        public PartedIngredientList() : base() 
+        private DataGridView grid_view;
+        private string? path;
+        private Report? selected_report;
+
+        public PartedIngredientList(ref DataGridView grid_view) : base() 
         {
             this.Rows = new List<Row>();
+            this.Reports = new List<Report>();
+
+            this.grid_view = grid_view;
 
             this.Columns.Add("No.");
             this.Columns.Add("이름");
@@ -31,11 +70,15 @@ namespace GBTH.List
             this.Columns.Add("분류");
 
             this.View = View.Details;
+            this.FullRowSelect = true;
+
+            this.ItemSelectionChanged += IndexChanged;
         }
 
         public void Add(Row row)
         {
             ListViewItem item = new ListViewItem(row.Number.ToString());
+
             item.SubItems.Add(row.Name);
             item.SubItems.Add(row.Standard);
             item.SubItems.Add(row.Unit);
@@ -45,38 +88,171 @@ namespace GBTH.List
             this.Items.Add(item);
         }
 
-        public void Serialize(string path_of_folder, int year)
+        private void IndexChanged(object? sender, EventArgs e)
         {
-            string result = JsonSerializer.Serialize(this.Rows);
-
-            if (!Directory.Exists(path_of_folder))
+            if (this.SelectedItems.Count > 0)
             {
-                Directory.CreateDirectory(path_of_folder);
+                if (this.selected_report != null)
+                {
+                    SaveData();
+                }
+
+                this.selected_report = this.Reports.Find((x) => { return int.Parse(this.SelectedItems[0].Text) == x.Number; });
+
+                if (this.selected_report == null)
+                {
+                    this.selected_report = new Report()
+                    {
+                        Number = int.Parse(this.SelectedItems[0].Text),
+                        Name = this.SelectedItems[0].SubItems[0].Text,
+                        Standard = this.SelectedItems[0].SubItems[1].Text,
+                    };
+
+                    this.Reports.Add(this.selected_report);
+                }
+
+                LoadData();
+            }
+        }
+
+        private void LoadData()
+        {
+            this.grid_view.Rows.Clear();
+
+            for (int i = 0; i < this.selected_report!.Rows.Count; i++)
+            {
+                this.grid_view.Rows.Add
+                    (
+                        this.selected_report.Rows[i].Number,
+                        this.selected_report.Rows[i].Date,
+                        this.selected_report.Rows[i].Descryption,
+                        this.selected_report.Rows[i].Insert,
+                        this.selected_report.Rows[i].Use,
+                        this.selected_report.Rows[i].Storege,
+                        this.selected_report.Rows[i].Manager,
+                        this.selected_report.Rows[i].Other
+                    );
+            }
+        }
+
+        public void SaveData()
+        {
+            if (this.selected_report != null)
+            {
+                this.selected_report!.Rows.Clear();
+
+                for (int i = 0; i < this.grid_view.RowCount - 1; i++)
+                {
+                    this.selected_report!.Rows.Add(new ReportRow
+                        (
+                            int.Parse((this.grid_view.Rows[i].Cells[0].EditedFormattedValue as string == "" ? "0" 
+                                : this.grid_view.Rows[i].Cells[0].EditedFormattedValue as string)!),
+                            this.grid_view.Rows[i].Cells[1].EditedFormattedValue as string,
+                            this.grid_view.Rows[i].Cells[2].EditedFormattedValue as string,
+                            int.Parse((this.grid_view.Rows[i].Cells[3].EditedFormattedValue as string == "" ? "0"
+                                : this.grid_view.Rows[i].Cells[3].EditedFormattedValue as string)!),
+                            int.Parse((this.grid_view.Rows[i].Cells[4].EditedFormattedValue as string == "" ? "0"
+                                : this.grid_view.Rows[i].Cells[4].EditedFormattedValue as string)!),
+                            int.Parse((this.grid_view.Rows[i].Cells[5].EditedFormattedValue as string == "" ? "0"
+                                : this.grid_view.Rows[i].Cells[5].EditedFormattedValue as string)!),
+                            this.grid_view.Rows[i].Cells[6].EditedFormattedValue as string,
+                            this.grid_view.Rows[i].Cells[1].EditedFormattedValue as string
+                        ));
+                }
+
+                Serialize();
+            }
+        }
+
+        public void Serialize()
+        {
+            string result = JsonSerializer.Serialize(this.Rows) + "\r" + JsonSerializer.Serialize(this.Reports);
+            
+            if (this.path != null)
+            {
+                if (!Directory.Exists(this.path))
+                {
+                    Directory.CreateDirectory(this.path);
+                }
+            }
+            else
+            {
+                throw new Exception("This instance isn't have path");
             }
 
-            StreamWriter writer = new StreamWriter(path_of_folder + "\\" + year + "-" + Properties.Resources.PartedIngredientPath);
+            StreamWriter writer = new StreamWriter(this.path + "\\" + Properties.Resources.PartedIngredientPath);
+            writer.Write(result);
+            writer.Close();
+        }
+        public void Serialize(string path_of_folder, int year)
+        {
+            string result = JsonSerializer.Serialize(this.Rows) + "\r" + JsonSerializer.Serialize(this.Reports);
+
+            if (!Directory.Exists(path_of_folder + "\\" + year))
+            {
+                Directory.CreateDirectory(path_of_folder + "\\" + year);
+            }
+
+            StreamWriter writer = new StreamWriter(path_of_folder + "\\" + year + "\\" + Properties.Resources.PartedIngredientPath);
             writer.Write(result);
             writer.Close();
         }
 
-        public static PartedIngredientList Deserialize(string path_of_folder, int year)
+        public void PartedDeserialize()
         {
-            if (!Directory.Exists(path_of_folder))
+            EB.Excel.Stream stream = new EB.Excel.Stream(Environment.CurrentDirectory + "\\default.xlsx");
+            string[,] cells = stream.Read(stream.UsedRange);
+
+            for (int r = 0; r < cells.GetLength(0); r++)
             {
-                throw new Exception("Not found path: (" + path_of_folder + ")");
+                this.Rows.Add(new Row()
+                {
+                    Number = int.Parse("0" + cells[r, 0]),
+                    Name = cells[r, 1],
+                    Standard = cells[r, 2],
+                    Unit = cells[r, 3],
+                    Assortment = cells[r, 4],
+                });
             }
 
-            StreamReader reader = new StreamReader(path_of_folder + "\\" + year + "-" + Properties.Resources.PartedIngredientPath);
+            string result = JsonSerializer.Serialize(this.Rows) + "\r" + JsonSerializer.Serialize(this.Reports);
+
+            if (!Directory.Exists(this.path))
+            {
+                Directory.CreateDirectory(this.path!);
+            }
+
+            StreamWriter writer = new StreamWriter(this.path + "\\" + Properties.Resources.PartedIngredientPath);
+            writer.Write(result);
+            writer.Close();
+        }
+
+        public static PartedIngredientList Deserialize(ref DataGridView view, string path_of_folder, int year)
+        {
+            if (!Directory.Exists(path_of_folder + "\\" + year))
+            {
+                PartedIngredientList result = new PartedIngredientList(ref view)
+                {
+                    path = path_of_folder + "\\" + year,
+                };
+                result.PartedDeserialize();
+            }
+            StreamReader reader = new StreamReader(path_of_folder + "\\" + year + "\\" + Properties.Resources.PartedIngredientPath);
             string json = reader.ReadToEnd();
+            string row_json = json.Split('\r')[0];
+            string report_json = json.Split('\r')[1];
             reader.Close();
 
-            List<Row>? rows = JsonSerializer.Deserialize<List<Row>>(json);
+            List<Row>? rows = JsonSerializer.Deserialize<List<Row>>(row_json);
+            List<Report>? reports = JsonSerializer.Deserialize<List<Report>>(report_json);
 
-            if (rows != null)
+            if (rows != null && reports != null)
             {
-                PartedIngredientList result = new PartedIngredientList()
+                PartedIngredientList result = new PartedIngredientList(ref view)
                 {
                     Rows = rows,
+                    path = path_of_folder + "\\" + year,
+                    Reports = reports,
                 };
 
                 foreach (Row row in rows)
@@ -84,9 +260,9 @@ namespace GBTH.List
                     ListViewItem item = new ListViewItem(row.Number.ToString());
                     item.SubItems.Add(row.Name);
                     item.SubItems.Add(row.Standard);
-                    item.SubItems.Add(row.Unit); 
+                    item.SubItems.Add(row.Unit);
                     item.SubItems.Add(row.Assortment);
-                    
+
                     result.Items.Add(item);
                 }
 
--- GBTH/MainForm.cs
+++ GBTH/MainForm.cs
@@ -7,11 +7,13 @@ namespace GBTH
         private SplitContainer tool_container;
         private SplitContainer container;
         private PartedIngredientList list_view;
-        private DataGridView grid;
+        private DataGridView grid_view;
+        private NumericUpDown numeric;
 
         public MainForm()
         {
             this.WindowState = FormWindowState.Maximized;
+            this.FormClosed += MainFormFormClosed;
 
             InitializeComponent();
 
@@ -23,6 +25,7 @@ namespace GBTH
                 Orientation = Orientation.Horizontal,
                 Dock = DockStyle.Fill,
             };
+
             this.container = new SplitContainer()
             {
                 Parent = this.tool_container.Panel1,
@@ -30,41 +33,99 @@ namespace GBTH
                 Size = this.tool_container.Panel1.ClientSize,
                 Dock = DockStyle.Fill,
             };
-            this.list_view = new PartedIngredientList()
-            {
-                Parent = this.container.Panel1,
-                Visible = true,
-                Size = this.container.Panel1.ClientSize,
-                Dock = DockStyle.Fill,
-            };
-            this.grid = new DataGridView()
+
+            this.grid_view = new DataGridView()
             {
                 Parent = this.container.Panel2,
                 Visible = true,
                 Size = this.container.Panel2.ClientSize,
                 Dock = DockStyle.Fill,
             };
+            this.grid_view.Columns.Add("Number", "번호"); //0
+            this.grid_view.Columns.Add("Date", "날짜");
+            this.grid_view.Columns.Add("Descrytion", "내용");
+            this.grid_view.Columns.Add("Insert", "입고"); //3
+            this.grid_view.Columns.Add("Use", "사용"); //4
+            this.grid_view.Columns.Add("Storege", "재고"); //5
+            this.grid_view.Columns.Add("Manager", "담당자");
+            this.grid_view.Columns.Add("Other", "비고");
 
-            //List.IngredientList list = new GBTH.List.IngredientList();
-            //list.Rows.Add(new GBTH.List.IngredientList.Row() {Year = 2020, ListPath = "hi", ReportPath = "Hello" });
-            //list.Serialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-            //IngredientList news = IngredientList.Deserialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            this.grid_view.Columns[0].ValueType = typeof(int);
+            this.grid_view.Columns[3].ValueType = typeof(int);
+            this.grid_view.Columns[4].ValueType = typeof(int);
+            this.grid_view.Columns[5].ValueType = typeof(int);
+            this.grid_view.Columns[5].ReadOnly = true;
 
-            //this.list_view.Add(new PartedIngredientList.Row() { Number = 1, Name = "A" });
-            //this.list_view.Add(new PartedIngredientList.Row() { Number = 2, Name = "B" });
-            //this.list_view.Add(new PartedIngredientList.Row() { Number = 3, Name = "C" });
-            //this.list_view.Serialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 2020);
+            this.grid_view.Click += (sender, e) =>
+            {
+                this.grid_view.Rows[0].Cells[5].Value 
+                    = int.Parse((this.grid_view.Rows[0].Cells[3].EditedFormattedValue as string == "" ? "0"
+                        : this.grid_view.Rows[0].Cells[3].EditedFormattedValue as string)!);
 
-            //this.list_view = PartedIngredientList.Deserialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 2020);
-            //this.list_view.Parent = this.container.Panel1;
-            //this.list_view.Visible = true;
-            //this.list_view.Size = this.container.Panel1.ClientSize;
-            //this.list_view.Dock = DockStyle.Fill;
+                for (int r = 1; r < this.grid_view.RowCount; r++)
+                {
+                    this.grid_view.Rows[r].Cells[5].Value
+                        = int.Parse((this.grid_view.Rows[r - 1].Cells[5].EditedFormattedValue as string == "" ? "0"
+                            : this.grid_view.Rows[r - 1].Cells[5].EditedFormattedValue as string)!)
+                        - int.Parse((this.grid_view.Rows[r - 1].Cells[4].EditedFormattedValue as string == "" ? "0"
+                            : this.grid_view.Rows[r - 1].Cells[4].EditedFormattedValue as string)!)
+                        + int.Parse((this.grid_view.Rows[r].Cells[3].EditedFormattedValue as string == "" ? "0"
+                            : this.grid_view.Rows[r].Cells[3].EditedFormattedValue as string)!);
+                }
+            };
+
+            this.numeric = new NumericUpDown()
+            {
+                Parent = this.tool_container.Panel2,
+                Visible = true,
+                Minimum = 2000,
+                Maximum = 3000,
+                Value = DateTime.Now.Year,
+                Increment = 1,
+            };
+            this.numeric.ValueChanged += NumericValueChanged;
+            {
+                //this.list_view = new PartedIngredientList(ref this.grid)
+                //{
+                //    Parent = this.container.Panel1,
+                //    Visible = true,
+                //    Size = this.container.Panel1.ClientSize,
+                //    Dock = DockStyle.Fill,
+                //};
+
+                //List.IngredientList list = new GBTH.List.IngredientList();
+                //list.Rows.Add(new GBTH.List.IngredientList.Row() {Year = 2020, ListPath = "hi", ReportPath = "Hello" });
+                //list.Serialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+                //IngredientList news = IngredientList.Deserialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+                //this.list_view.Add(new PartedIngredientList.Row() { Number = 1, Name = "A" });
+                //this.list_view.Add(new PartedIngredientList.Row() { Number = 2, Name = "B" });
+                //this.list_view.Add(new PartedIngredientList.Row() { Number = 3, Name = "C" });
+                //this.list_view.Serialize(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 2020);
+            }
+
+            this.list_view = PartedIngredientList.Deserialize(ref this.grid_view, Environment.CurrentDirectory, (int)this.numeric.Value);
+            this.list_view.Parent = this.container.Panel1;
+            this.list_view.Visible = true;
+            this.list_view.Size = this.container.Panel1.ClientSize;
+            this.list_view.Dock = DockStyle.Fill;
         }
 
-        private void ResizeAll(object? sender, EventArgs e)
+        private void NumericValueChanged(object? sender, EventArgs e)
         {
+            this.list_view.Visible = false;
+            this.list_view.Dispose();
 
+            this.list_view = PartedIngredientList.Deserialize(ref this.grid_view, Environment.CurrentDirectory, (int)this.numeric.Value);
+            this.list_view.Parent = this.container.Panel1;
+            this.list_view.Visible = true;
+            this.list_view.Size = this.container.Panel1.ClientSize;
+            this.list_view.Dock = DockStyle.Fill;
+        }
+
+        private void MainFormFormClosed(object? sender, FormClosedEventArgs e)
+        {
+            this.list_view.SaveData();
         }
     }
 }
